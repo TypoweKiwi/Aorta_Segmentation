5c0759d33fbb6dfa8b7762e76bafd9f10eec26b1
TypoweKiwi <wiktorkilian@o2.pl>
2025-08-03 23:47:45 +0200

    Updating data loader / adjusting data loader structure

--- Dataset_loader.py
+++ Dataset_loader.py
@@ -12,47 +12,43 @@ class Dataset(tc.utils.data.Dataset):
         self.borders = []
 
         slice_index = 0
-
         for name in data_names:
             self.borders.append("First slice of " + os.path.basename(name) + ": " + str(slice_index))
-
-            image = sitk.ReadImage(name)
-            image = sitk.GetArrayFromImage(image).T
-            if image.shape[1] == 666:   #Normalize shape of data
-                image = image[:, 77:589, :]
+            image = self.load_image(name=name)
+            mask = self.load_image(name=name, load_mask=True)
             for i in range(image.shape[2]):
-                self.images.append(image[:, :, i])   #Adding every slice separately
+                self.images.append(cv2.resize(image[:, :, i], (224, 224)))   #Adding every slice separately
                 self.images_names.append(os.path.basename(name) + "Slice" + str(i))
+
+                resized_mask = cv2.resize((mask[:, :, i] > 0).astype(np.float32), (224, 224)) #We apply > 0 condition to assure that mask has True or False value (1 or 0)
+                self.masks.append(tc.from_numpy(resized_mask )) 
                 slice_index += 1
 
-            mask_path = name.replace(".nrrd", ".seg.nrrd")   #The masks have the same name as the files but their format is different
-            if not os.path.exists(mask_path):
-                raise ValueError(f"There is no mask for file {os.path.basename(name)}")
-
-            mask = sitk.ReadImage(mask_path)
-            mask = sitk.GetArrayFromImage(mask).T
-            if mask.shape[1] == 666:   #Normalize shape of mask
-                mask = mask[:, 77:589, :]
-            for i in range(mask.shape[2]):
-                self.masks.append(mask[:, :, i])   #To match slices of data we also slice masks
-
-    def Preprocess_Data(self):
-        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(4, 4))
+    def preprocess_data(self):
         for i in range(len(self.images)):
-            image = self.images[i]
-            if "R" in self.images_names[i]:   #Rider files have NaN values that are read as -2000
-                image[image == -2000] = 0
-            image = (image - np.min(image)) / (np.max(image) - np.min(image))   #Normalize to 0-1 range
-            image = np.uint8(255*(image))   #Scale to uint8 in 0-255 range for CV2 functions
-            image = cv2.resize(image, (224, 224))
-            image = clahe.apply(image)   #Clahe increase the contrast
+            image = self.apply_preprocesing_functions(self.images[i])
             self.images[i] = tc.from_numpy(np.float32(image)/255) #Preparing data so that the model can process it effectively
-
-        for i in range(len(self.masks)):
-            mask = self.masks[i]
-            mask = cv2.resize(mask, (224, 224))
-            mask = (mask > 0)
-            self.masks[i] = tc.from_numpy(np.float32(mask))
+    
+    def load_image(self, name, load_mask=False):
+        if load_mask:
+            name = name.replace(".nrrd", ".seg.nrrd")
+        image = sitk.ReadImage(name)
+        image = sitk.GetArrayFromImage(image).T.astype(np.float32)
+        if image.shape[1] == 666:   #Normalize shape of data
+            image = image[:, 77:589, :]
+        if "Dongyang" in name and not load_mask: #Normalization of background - to see more, look into data_overview.ipynb
+            image = image + 1024
+        else:
+            image[image < 0] = 0
+        return image
+    
+    def apply_preprocesing_functions(self, image):
+        clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(4, 4))
+        image = (image - np.min(image)) / (np.max(image) - np.min(image))   
+        image = np.uint8(255*(image))   
+        image = clahe.apply(image)   
+        image = cv2.medianBlur(image, 3)
+        return image
 
     def __len__(self):
         return len(self.images)
@@ -60,8 +56,8 @@ class Dataset(tc.utils.data.Dataset):
     def __getitem__(self, index):
         return self.images[index], self.masks[index]
 
-    def Get_name(self, index):
+    def get_name(self, index):
         return self.images_names[index]
 
-    def Get_borders(self):
+    def get_borders(self):
         print(self.borders)
